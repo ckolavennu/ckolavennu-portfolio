0e96b1b8603a9e34dcf2648f7819ee5527a0c081
Chandu Kolavennu <56797696+ckolavennu@users.noreply.github.com>
2026-06-14 20:25:49 +0800

    Import project status pill colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import '$lib/styles/status-pills.css';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 </script>
