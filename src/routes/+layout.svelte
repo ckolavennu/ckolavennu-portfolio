c0f8038c5e9f26230db2d743dcfcfa6dc491a411
Chandu Kolavennu <56797696+ckolavennu@users.noreply.github.com>
2026-07-23 09:09:31 +0800

    Import landing background overrides

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import '$lib/styles/status-pills.css';
+	import '$lib/styles/landing-background.css';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 </script>
